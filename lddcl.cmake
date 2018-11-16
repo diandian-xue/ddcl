d4609807167e7f4cc8405a6ca0e481c207d30a27
diandian <ddgame3d@live.com>
2018-11-16 16:53:57 +0800

    modify cmake

--- lddcl.cmake
+++ lddcl.cmake
@@ -8,27 +8,21 @@ add_library(lddcl
     ${SOURCES}
     ${HEADERS}
     )
-add_executable(lddcl_execute
-    ${SOURCES}
-    ${HEADERS}
-    ${SRC_PATH}/main/lmain.c
-    )
 
 
 if(${BUILD_SHARED_LIBS})
     target_compile_definitions(lddcl PUBLIC DDCLLUA_EXPORTS)
-    #target_compile_definitions(lddcl_execute PUBLIC DDCLLUA_EXPORTS)
 endif()
 
+target_include_directories(lddcl PUBLIC ${SRC_PATH})
+set_target_properties(lddcl PROPERTIES PREFIX "")
+set_target_properties(lddcl PROPERTIES OUTPUT_NAME lddcl)
+target_link_libraries(lddcl PUBLIC lualib)
 target_link_libraries(lddcl PUBLIC ddcl)
-target_link_libraries(lddcl_execute PUBLIC ddcl)
-target_include_directories(lddcl_execute PUBLIC ${SRC_PATH})
 
-if(WIN32)
-else()
+if(LINUX)
     set(DDCLLUA_COMPILE_FLAGS -std=gnu99)
     set_target_properties(lddcl PROPERTIES COMPILE_FLAGS ${DDCLLUA_COMPILE_FLAGS})
-    set_target_properties(lddcl_execute PROPERTIES COMPILE_FLAGS ${DDCLLUA_COMPILE_FLAGS})
 endif()
 
 if(APPLE)
@@ -37,11 +31,20 @@ if(APPLE)
     endif()
 endif()
 
-set_target_properties(lddcl PROPERTIES PREFIX "")
-set_target_properties(lddcl PROPERTIES OUTPUT_NAME lddcl)
+
+add_executable(lddcl_execute
+    ${SRC_PATH}/main/lmain.c
+    )
+
+target_link_libraries(lddcl_execute PUBLIC ddcl)
+target_link_libraries(lddcl_execute PUBLIC lualib)
+target_link_libraries(lddcl_execute PUBLIC lddcl)
+target_link_libraries(lddcl_execute PUBLIC lua-cjson)
 set_target_properties(lddcl_execute PROPERTIES OUTPUT_NAME lddcl)
 
-#target_include_directories(lddcl PUBLIC ${PROJECT_SOURCE_DIR}/3rd/lua/src)
-target_link_libraries(lddcl PUBLIC lualib)
-target_link_libraries(lddcl_execute PUBLIC lualib)
-target_link_libraries(lddcl_execute PUBLIC lua-cjson)
+if(WIN32)
+else()
+    set_target_properties(lddcl_execute
+        PROPERTIES COMPILE_FLAGS ${DDCLLUA_COMPILE_FLAGS})
+endif()
+
